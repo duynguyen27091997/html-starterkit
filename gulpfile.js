8e8b5ebf140d258ed18513459b1ce5671f9acab2
duynn <ngocduy@tuoitre.com.vn>
2021-03-30 13:41:09 +0700

    useref

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,13 @@ const {series, src, dest, parallel, watch} = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache')
+    cache = require('gulp-cache'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-clean-css'),
+    replace = require('gulp-replace');
 
 
 const paths = {
@@ -35,15 +38,19 @@ const paths = {
             files: './src/scss/**/*',
             main: './src/scss/*.scss'
         },
+        css: {
+            dir: './src/css',
+            files: './src/css/**/*',
+            main: './src/css/*.css'
+        },
         pug: {
-            dir: './src/pug',
-            files: './src/pug/**/*',
-            main: './src/pug/*.pug'
+            dir: './src/views',
+            files: './src/views/**/*',
+            main: './src/views/*.pug'
         },
         js: {
             main: './src/js/*.js',
-            pages: './src/js/pages',
-            files: './src/js/pages/*.js',
+            files: './src/js/**/*',
             dir: './src/js'
         },
         images: {
@@ -53,6 +60,9 @@ const paths = {
         fonts: {
             dir: './src/fonts',
             files: './src/fonts/**/*'
+        },
+        libs: {
+            dir: './src/libs'
         },
     },
     dist: {
@@ -78,18 +88,25 @@ const paths = {
         },
     }
 }
+
 const cleanLock = (callback) => {
     del.sync(paths.base.lock.files)
     callback()
 }
 const cleanDist = (callback) => {
-    del.sync(paths.dist.libs.dir)
+    del.sync(paths.dist.base.dir)
+    callback()
+}
+
+const cleanSrcBuild = (callback) => {
+    del.sync(paths.src.css.dir)
+    del.sync(paths.src.libs.dir)
     callback()
 }
 const browserSyncInit = (callback) => {
     browserSync.init({
         server: {
-            baseDir: [paths.dist.base.dir, paths.src.base.dir, paths.base.base.dir],
+            baseDir: [paths.src.base.dir],
         }
     })
     callback()
@@ -100,8 +117,7 @@ const browserSyncReload = (callback) => {
 }
 const serve = () => {
     watch(paths.src.scss.files, series(compileScss, browserSyncReload))
-    watch([paths.src.js.dir], series(compileJs, browserSyncReload))
-    watch([paths.src.js.pages], series(compilePageJs, browserSyncReload))
+    watch(paths.src.js.files, series(browserSyncReload))
     watch(paths.src.pug.files, series(compilePug, browserSyncReload))
 }
 
@@ -111,35 +127,49 @@ const compilePug = () => src(paths.src.pug.main, {
     .pipe(pug({
         pretty: true
     }))
-    .pipe(dest(paths.dist.base.dir))
+    .pipe(dest(paths.src.base.dir))
 
 const compileScss = () => src(paths.src.scss.main)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(dest(paths.dist.css.dir))
-    .pipe(cleanCSS())
     .pipe(
         rename({
             suffix: ".min"
         })
     )
     .pipe(sourcemaps.write("./"))
-    .pipe(dest(paths.dist.css.dir));
+    .pipe(dest(paths.src.css.dir));
 
-const compileJs = () => src(paths.src.js.main)
-    .pipe(uglify())
-    .pipe(dest(paths.dist.js.dir));
+const buildScss = () => src(paths.src.scss.main)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(replace(/url\((?:"|'|)([\w\d_\-\/.]+.(?:png|svg|jpg|jpeg|gif|ttf|eot|eot\?#iefix|woff2|woff))(?:"|'|)\)/gi, function (match, p1) {
+        let imgPath = p1.replace(/\.\.\//g, '')
+        return `url(../${imgPath})`;
+    }))
+    .pipe(
+        rename({
+            suffix: ".min"
+        })
+    )
+    .pipe(sourcemaps.write("./"))
+    .pipe(dest(paths.src.css.dir));
 
-const compilePageJs = () => src(paths.src.js.files)
-    .pipe(uglify())
-    .pipe(dest(paths.dist.js.pages));
+// const compileJs = () => src(paths.src.js.main)
+//     .pipe(uglify())
+//     .pipe(dest(paths.dist.js.dir));
+//
+// const compilePageJs = () => src(paths.src.js.files)
+//     .pipe(uglify())
+//     .pipe(dest(paths.dist.js.pages));
 
 const copyLibs = () => src(npmDist(), {
     base: paths.base.node.dir
 }).pipe(rename((path) => {
     path.dirname = path.dirname.replace(/\/dist/, '').replace(/\\dist/, '');
-})).pipe(dest(paths.dist.libs.dir));
+})).pipe(dest(paths.src.libs.dir));
 
 const copyImages = () => src(`${paths.src.images.files}.+(png|jpg|jpeg|gif|svg)`)
     .pipe(cache(imagemin({
@@ -151,15 +181,19 @@ const copyFonts = () => src(`${paths.src.fonts.files}`)
     .pipe(dest(paths.dist.fonts.dir))
 
 
+const useRef = () => src(`${paths.src.base.dir}/*.html`)
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(dest(paths.dist.base.dir));
 
 exports.build = series(
-    parallel(cleanLock, cleanDist, copyLibs),
-    compileScss,
-    compileJs,
-    compilePageJs,
-    compilePug,
+    parallel(cleanLock, cleanDist),
+    parallel(buildScss, copyImages, copyFonts),
+    series(compilePug, useRef),
 )
 exports.default = series(
-    parallel(cleanLock, cleanDist, copyLibs,copyImages,copyFonts, compileScss, compileJs, compilePageJs, compilePug),
+    parallel(cleanLock, cleanSrcBuild, copyLibs, compileScss),
+    compilePug,
     parallel(browserSyncInit, serve)
 )
